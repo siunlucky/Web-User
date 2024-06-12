32eec8efe6ea32710dcf1dd51b064d6cfa5cc5f5
ZGE26 <aryaersiputra8@gmail.com>
2024-06-12 14:38:57 +0700

    Halaman Login

---
+++ src/app/logo/page.js
@@ -0,0 +1,15 @@
+// components/Logo.js
+import Image from 'next/image';
+import logo from './Logo.png'; // Import gambar sebagai modul
+import './logo.css';
+
+const Logo = () => {
+  return (
+    <div className="logo">
+      <Image src={logo} alt="Logo" width={200} height={100} className="logo-img" />
+      <h1>Welcome To HomeVice!</h1>
+    </div>
+  );
+};
+
+export default Logo;
